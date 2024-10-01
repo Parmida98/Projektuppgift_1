676dd253a2daa9d8397fba1feb302677703139cc
Parmida98 <Parmida.ahmadi@stud.sti.se>
2024-10-01 17:11:10 +0200

    update dice

--- src/se_parmida/Dice.java
+++ src/se_parmida/Dice.java
@@ -1,4 +1,23 @@
 package se_parmida;
 
+import java.util.Random;
+
 public class Dice {
+    private Random random;
+    private int result;
+
+
+    public Dice() {
+        this.random = new Random();
+        this.result = 0;
+
+    }
+
+
+    public int rollDice() {
+        this.result = this.random.nextInt(6) + 1;
+        return this.result;
+
+    }
 }
+
